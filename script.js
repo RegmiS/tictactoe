bb2b5f378d14c145838f3588e4e6c158b08a0c38
RegmiS <sandeshreg35@gmail.com>
2024-09-24 17:48:44 -0500

    Add playerstate switching and gameboard updating

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 // 9 spots, 2 players, board with spot and either blank or not blank
 // 3 ways across to win, 3 ways down to win, 2 diagonals to win
 
-const board = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]];
-const playerState = "X";
+const board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+let playerState = "X";
 const boardDisplay = document.querySelector('.gameBoard');
+const curplayer = document.querySelector('#gamestate');
 console.table(board);
 
-function gameButtonClick(gameIndex){
-    console.log(gameIndex[0], gameIndex[1]);
+function updateCurPlayer(){
+    const playerStatement = playerState=="X"? "Player1  X" : "Player2 O";
+    curplayer.innerHTML = "Cur Player: " + playerStatement;
 }
 
-
+function gameButtonClick(x,y){
+    console.log(x, y);
+    if (board[x][y]== " "){
+        board[x][y] = playerState;
+        playerState = (playerState=="X") ? "O":"X";
+        updateCurPlayer();
+        displayGame();
+    }
+}
 
 function displayGame (){
+    boardDisplay.innerHTML = "";
     for(let x = 0; x < board.length; x++){
         for(let y = 0; y < board[0].length; y++){
             const boardButton = document.createElement('button');
             boardButton.classList = "gamebutton";
-            boardButton.innerHTML = "X";
+            boardButton.innerHTML = board[x][y];
             boardButton.addEventListener("click", (event) => {
-                gameButtonClick([x,y]);
+                gameButtonClick(x,y);
                 event.preventDefault();
             });
             boardDisplay.appendChild(boardButton);
@@ -27,10 +38,6 @@ function displayGame (){
     }
 }
 
-const curplayer = document.querySelector('#gamestate');
 
-
-const playerStatement = playerState=="X"? "Player1  X" : "Player2 O";
-curplayer.innerHTML = "Cur Player: " + playerStatement;
-
+updateCurPlayer();
 displayGame();
