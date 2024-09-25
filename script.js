09445b35be72334cbe9c419a102d5067e92ee8ec
RegmiS <sandeshreg35@gmail.com>
2024-09-24 19:47:18 -0500

    Add win state checker and displayer

--- script.js
+++ script.js
@@ -7,18 +7,51 @@ const boardDisplay = document.querySelector('.gameBoard');
 const curplayer = document.querySelector('#gamestate');
 console.table(board);
 
+
+function checkWinner() {
+    // if rows/cols/dia equal to each other, that player wins
+    let win = false;
+    let winPlayer = "";
+    for(let x = 0; x < board.length; x++){
+        if (board[x][0]==board[x][1] && board[x][1] == board[x][2] && board[x][2] == playerState) {
+            winPlayer=board[x][0];
+            win = true;
+        }
+        if (board[0][x]==board[1][x] && board[1][x]==board[2][x] && board[2][x]==playerState) {
+            winPlayer=board[0][x];
+            win = true;
+        }
+    }
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[2][2]==playerState){
+        winPlayer = board[0][0];
+        win=true;
+    }
+    if(board[2][0]==board[1][1] && board[1][1]==board[0][2] && board[0][2]==playerState){
+        winPlayer = board[2][0];
+        win=true;
+    }
+    if(win==true){
+        const winPar = document.createElement('p');
+        const playerStatement = playerState=="X"? "Player1  X" : "Player2 O";
+        winPar.innerHTML = "Finished game, winner is " + playerStatement;
+        boardDisplay.innerHTML = "";
+        boardDisplay.appendChild(winPar);
+    }
+}
+
 function updateCurPlayer(){
     const playerStatement = playerState=="X"? "Player1  X" : "Player2 O";
     curplayer.innerHTML = "Cur Player: " + playerStatement;
 }
 
 function gameButtonClick(x,y){
-    console.log(x, y);
+    // console.log(x, y);
     if (board[x][y]== " "){
         board[x][y] = playerState;
+        displayGame();
+        checkWinner();
         playerState = (playerState=="X") ? "O":"X";
         updateCurPlayer();
-        displayGame();
     }
 }
 
