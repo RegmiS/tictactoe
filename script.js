39a5b3d2dcee1c45b8b9d218441f44c56dc3bbff
RegmiS <sandeshreg35@gmail.com>
2024-10-06 21:51:04 -0500

    Add player type selection binding and clicking

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// handles the gameboard content and getting/updating/resetting gameboard values
 const gameBoard = (function(){
     let gameBoardArray = [["","",""], ["","",""], ["","",""]];
     
@@ -5,21 +7,36 @@ const gameBoard = (function(){
         gameBoardArray[row][col] = playerState;
     };
 
-
+    // todo:
+    // 
 
     return { updateGameBoard };
 
 })();
 
+
+// handles changing of gamestate - checks, player checks, game functions
 const gamestateController = (() => {
+    let playerState = "X";
+
+    const swapState = () => {
+        let oldState = playerState;
+        playerState = oldState == "X"? "O":"X";
+        const oldStateDoc = document.querySelector(`#${oldState}`);
+        const newStateDoc = document.querySelector(`#${playerState}`);
+        oldStateDoc.classList = "player-btn";
+        newStateDoc.classList = "player-btn selected";
+    }
+
     const test = (index) => {
         console.log(`test ${index}`);
     }
     
-    return {test};
+    return {test, swapState};
     
 })();
 
+// handles button clicking/page interactions
 const displayController = (function(){
     const htmlBoard = Array.from(document.querySelectorAll('button.board-btn'));
 
@@ -32,6 +49,13 @@ const displayController = (function(){
             const element = htmlBoard[index];
             element.addEventListener('click', gamestateController.test.bind(element, index));
         }
+    })();
+
+    const _bindPlayerBtn = (() => {
+        const xselect = document.querySelector(`#X`);
+        xselect.addEventListener('click', gamestateController.swapState.bind(xselect));
+        const oselect = document.querySelector(`#O`);
+        oselect.addEventListener('click', gamestateController.swapState.bind(oselect));
     })();
     
 })();
