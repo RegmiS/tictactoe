4275ca6a08b10e6b937da7d33f4fd40f03c89778
RegmiS <sandeshreg35@gmail.com>
2024-10-06 21:01:33 -0500

    Updating selection css, factory function button binding

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ const gameBoard = (function(){
         gameBoardArray[row][col] = playerState;
     };
 
-    
+
 
     return { updateGameBoard };
 
 })();
+
+const gamestateController = (() => {
+    const test = (index) => {
+        console.log(`test ${index}`);
+    }
+    
+    return {test};
+    
+})();
+
+const displayController = (function(){
+    const htmlBoard = Array.from(document.querySelectorAll('button.board-btn'));
+
+    const test = (index) => {
+        console.log(index);
+    }
+    
+    const _init = (() => {
+        for (let index = 0; index < htmlBoard.length; index++) {
+            const element = htmlBoard[index];
+            element.addEventListener('click', gamestateController.test.bind(element, index));
+        }
+    })();
+    
+})();
+
